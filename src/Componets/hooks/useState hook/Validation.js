2ac6c51a4c61c07af1d18e1d84417e0814b19305
mohdmazharuddin <106807606+mohdmazharuddin@users.noreply.github.com>
2023-02-14 13:13:49 +0530

    advance validation

--- src/Componets/hooks/useState hook/Validation.js
+++ src/Componets/hooks/useState hook/Validation.js
@@ -4,33 +4,64 @@ import { Form, Button } from "react-bootstrap";
 export const Validation = () => {
   const [fisrtName, setFirstName] = useState("");
   const [fisrtNameErr, setFirstNameErr] = useState("");
+
   const [lastName, setLastName] = useState("");
   const [lastNameErr, setLastNameErr] = useState("");
-  const [isSubmit, serIsSubmit] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [emailErr, setEmailErr] = useState("");
+
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const emailRegex =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
   const submit = (event) => {
     event.preventDefault();
+    valid();
+    if (valid().fisrtName && valid().lastName && valid().email) {
+      setIsSubmit(true);
+    } else {
+      setIsSubmit(false);
+    }
+  };
+  const valid = () => {
+    const valid = { fisrtName: false, lastName: false, email: false };
     if (fisrtName.length <= 0) {
       setFirstNameErr("this field is required");
-    } else  {
-      setFirstNameErr("");
-    }
-    if (fisrtName.length >= 15) {
+    } else if (fisrtName.length >= 15) {
       setFirstNameErr("length not be greater than 15");
-    }
-    if (fisrtName.length <= 5 && fisrtName.length !== 0) {
+    } else if (fisrtName.length <= 5 && fisrtName.length !== 0) {
       setFirstNameErr("length not be less than 5");
+    } else {
+      setFirstNameErr("");
+      valid.fisrtName = true;
     }
+
     if (lastName.length <= 0) {
       setLastNameErr("this field is required");
     } else {
       setLastNameErr("");
+      valid.lastName = true;
     }
+
+    if (!emailRegex.test(email)) {
+      setEmailErr("email is not valid");
+    } else {
+      setEmailErr("");
+      valid.email = true;
+    }
+
+    return valid;
   };
   const FirstNamechange = (event) => {
     setFirstName(event.target.value);
   };
   const lastNamechange = (event) => {
     setLastName(event.target.value);
+  };
+  const emailchange = (e) => {
+    setEmail(e.target.value);
   };
   return (
     <div>
@@ -53,6 +84,15 @@ export const Validation = () => {
             onChange={lastNamechange}
           />
           <p className="text-danger">{lastNameErr}</p>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>email</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter email address"
+            onChange={emailchange}
+          />
+          <p className="text-danger">{emailErr}</p>
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
